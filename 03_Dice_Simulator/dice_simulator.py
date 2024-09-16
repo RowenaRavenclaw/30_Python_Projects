bd1b3028e9919e15a24dc327898722c1669bb7bd
Teona <teona.odisharia@gmail.com>
2024-09-16 13:43:22 +0400

    dice roller

---
+++ 03_Dice_Simulator/dice_simulator.py
@@ -0,0 +1,31 @@
+import random
+
+
+def roll_dace(amount: int = 2) -> list[int]:
+    if amount <= 0:
+        raise ValueError
+
+    rolls: list[int] = []
+    for i in range(amount):
+        random_roll: int = random.randint(1, 6)
+        rolls.append(random_roll)
+
+    return rolls
+
+
+def main():
+    while True:
+        try:
+            user_input: str = input("How many dice would you like to roll: ")
+
+            if user_input.lower() == "exit":
+                print("Thank you for playing")
+                break
+            print(roll_dace(int(user_input)))
+
+        except ValueError:
+            print("please type valid Number")
+
+
+if __name__ == "__main__":
+    main()
