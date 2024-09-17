6293eff2e0f8ab91c0b8787f70300461a9041bc2
Teona <teona.odisharia@gmail.com>
2024-09-17 10:33:58 +0400

    Dice Solution

--- 03_Dice_Simulator/dice_simulator.py
+++ 03_Dice_Simulator/dice_simulator.py
@@ -16,12 +16,13 @@ def roll_dace(amount: int = 2) -> list[int]:
 def main():
     while True:
         try:
-            user_input: str = input("How many dice would you like to roll: ")
+            user_input: str = input("How many dice would you like to roll? ")
 
             if user_input.lower() == "exit":
                 print("Thank you for playing")
                 break
-            print(roll_dace(int(user_input)))
+            roll_list = roll_dace(int(user_input))
+            print(*roll_list, f"({sum(roll_list)})", sep=", ")
 
         except ValueError:
             print("please type valid Number")
